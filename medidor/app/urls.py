78117b2f6b55c515a49c599a5ab2326844fb42e9
LisaRamirez <160017350+LisaRamirez@users.noreply.github.com>
2025-01-29 01:49:29 -0300

    29 Lisa

--- medidor/app/urls.py
+++ medidor/app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from.views import home
+from.views import home, clientes, contacto, nosotros, recursos, soluciones
 
 urlpatterns = [
     path('', home, name="home"),
+    path('clientes/', clientes, name="clientes"),
+    path('contacto/', contacto, name="contacto"),
+    path('nosotros/', nosotros, name="nosotros"),
+    path('recursos/', recursos, name="recursos"),
+    path('soluciones/', soluciones, name="soluciones"),
 ]
 # Compare this snippet from medidor/app/models.py:
--- medidor/app/views.py
+++ medidor/app/views.py
@@ -6,3 +6,18 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'app/home.html')
 
+def clientes(request):
+    return render(request, 'app/clientes.html')
+
+def contacto(request):
+    return render(request, 'app/contacto.html')
+
+def nosotros(request):
+    return render(request, 'app/nosotros.html')
+
+def recursos(request):
+    return render(request, 'app/recursos.html')
+
+def soluciones(request):
+    return render(request, 'app/soluciones.html')
+
