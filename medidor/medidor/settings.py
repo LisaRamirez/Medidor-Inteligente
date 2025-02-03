337f682389caae970d6863e95303138c21970914
LisaRamirez <160017350+LisaRamirez@users.noreply.github.com>
2025-02-03 04:14:33 -0300

    organizar mediaquerys en responsive / iOs , android, complemento de información en : recursos, clientes, quienes somos, organizar css

--- medidor/medidor/settings.py
+++ medidor/medidor/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'app',
 ]
 
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-CL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
