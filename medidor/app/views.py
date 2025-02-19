179e2478c2ef2c636c4fe6e4b92af1f068ea9605
LisaRamirez <160017350+LisaRamirez@users.noreply.github.com>
2025-02-18 22:29:48 -0300

    contacto

--- medidor/app/views.py
+++ medidor/app/views.py
@@ -1,5 +1,7 @@
+import datetime
 import smtplib
 import os
+from django.http import Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from email.mime.text import MIMEText
@@ -12,34 +14,57 @@ from django.contrib import messages
 from smtplib import SMTPException
 
 # Create your views here.
+
+def apr(request):
+
+
+
+
+
+
+
+    return render(request, 'app/apr.html')
+
+def home(request):
+    
+    return render(request, 'app/home.html')
+
+
 def contacto(request):
+    try:
+        if request.method == 'POST':
+            name = request.POST['name']
+            email = request.POST['email']
+            phone = request.POST['phone']
+            ssr_apr = request.POST['ssr_apr']
+            cargo_apr = request.POST['cargo_apr']
+            comuna = request.POST['comuna']
+            cantidad = request.POST['cantidad']
+            fecha = datetime.datetime.now()
+            radio = request.POST['financiamiento']
+            message = request.POST['message']   
 
-    if request.method == 'POST':
-        name = request.POST['name']
-        email = request.POST['email']
-        phone = request.POST['phone']
-        ssr_apr = request.POST['ssr_apr']
-        cargo_apr = request.POST['cargo_apr']
-        comuna = request.POST['comuna']
-        cantidad = request.POST['cantidad']
-        fecha = request.POST['fecha']
-        radio = request.POST['financiamiento']
-        mensaje = request.POST['mensaje']
-
-       
-        contacto = Contacto(name=name, email=email, phone=phone, ssr_apr=ssr_apr, cargo_apr=cargo_apr, comuna=comuna, cantidad=cantidad, fecha=fecha, radio=radio, mensaje=mensaje)
-        contacto.save()
-        inicio = settings.EMAIL_HOST_USER
-        password = settings.EMAIL_HOST_PASSWORD
-        info = f"nombre: {name} \n phone: {phone} \n ssr_apr: {ssr_apr} \n cargo_apr: {cargo_apr} \n comuna: {comuna} \n cantidad: {cantidad} \n fecha: {fecha} \n radio: {radio} \n mensaje: {mensaje}"
-        cc_recipients =['cristobalfariasfredes@gmail.com','lisaisabelc19@gmail.com']
-
-        send_email('APR/SSR contacto', info, inicio, password, email, cc_recipients, 'smtp.gmail.com', 587)
+            contacto = Contacto(name=name, email=email, phone=phone, ssr_apr=ssr_apr, cargo_apr=cargo_apr, comuna=comuna, cantidad=cantidad, fecha=fecha, radio=radio, message=message)
+            contacto.save()
+            inicio = settings.EMAIL_HOST_USER
+            password = settings.EMAIL_HOST_PASSWORD
+            info = f"nombre: {name} \n phone: {phone} \n ssr_apr: {ssr_apr} \n cargo_apr: {cargo_apr} \n comuna: {comuna} \n cantidad: {cantidad} \n fecha: {fecha} \n radio: {radio} \n message: {message}"
+            cc_recipients =['','']
+            try:
+                send_email('APR/SSR contacto', info, inicio, password, email, cc_recipients, 'smtp.gmail.com', 587)
+                messages.success(request, 'Tu mensaje ha sido enviado correctamente')
+            except send_email.error as e:
+                print(f"Error: Email could not be sent. {e}")
+            raise Http404   
+    except Exception as e:
+            print(f"Unexpected error: {e}")
+            messages.error(request, 'Hubo un problema con tu solicitud.')
+            return redirect('app/404.html')  # Change to an appropriate error page
 
     return render(request, 'app/contacto.html')
 
 def send_email(subject, message, sender_email, sender_password, recipient_email,cc_emails, smtp_server, smtp_port):
-    try:
+    try:    
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.ehlo()
         server.starttls()
@@ -57,10 +82,6 @@ def send_email(subject, message, sender_email, sender_password, recipient_email,
         print(f"Error: Email could not be sent. {e}")
 
 
-def home(request):
-    
-    return render(request, 'app/home.html')
-
 def clientes(request):
     return render(request, 'app/clientes.html')
 
@@ -74,7 +95,6 @@ def recursos(request):
 def soluciones(request):
     return render(request, 'app/soluciones.html')
 
-def apr(request):
-    return render(request, 'app/apr.html')
+
 
 
