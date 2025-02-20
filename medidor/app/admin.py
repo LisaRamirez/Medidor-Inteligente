7501b1cd2c76d0ef949b9d1bb7344460ac4d7220
LisaRamirez <160017350+LisaRamirez@users.noreply.github.com>
2025-02-20 02:45:16 -0300

    apr/cliente

--- medidor/app/admin.py
+++ medidor/app/admin.py
@@ -3,14 +3,14 @@ from .models import Contacto, Apr
 # Register your models here.
 
 class ContactoAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'ssr_apr', 'cargo_apr', 'comuna', 'fecha', 'radio')
+    list_display = ('id','name', 'email', 'phone', 'ssr_apr', 'cargo_apr', 'comuna', 'fecha', 'radio')
     search_fields = ('name', 'comuna')
     list_filter = ['name']
     ordering = ('name', 'email', 'phone', 'ssr_apr', 'cargo_apr', 'comuna', 'fecha', 'radio')
     list_per_page = 10
 
 class AprAdmin(admin.ModelAdmin):
-    list_display = ('nameapr', 'comuna', 'region')
+    list_display = ('id','nameapr', 'comuna', 'region')
     search_fields = ('nameapr', 'comuna', 'region')
     list_filter = ('nameapr', 'comuna', 'region')
     ordering = ('nameapr', 'comuna', 'region')
