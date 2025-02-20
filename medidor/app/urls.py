266a30694cab30e0e7ad63613365fae4c6303c07
LisaRamirez <160017350+LisaRamirez@users.noreply.github.com>
2025-02-20 01:01:59 -0300

    20/clientes ok

--- medidor/app/urls.py
+++ medidor/app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from.views import home, clientes, contacto, nosotros, recursos, soluciones,apr
+from.views import home, clientes, contacto, nosotros, recursos, soluciones,apr,e404
 
 urlpatterns = [
     path('', home, name="home"),
@@ -8,6 +8,7 @@ urlpatterns = [
     path('nosotros/', nosotros, name="nosotros"),
     path('recursos/', recursos, name="recursos"),
     path('soluciones/', soluciones, name="soluciones"),
-    path('apr/', apr, name="apr"),
+    path('apr/<int:apr_id>/', apr, name="apr"),
+    path('e404/', e404, name="e404"),
 ]
 # Compare this snippet from medidor/app/models.py:
--- medidor/app/views.py
+++ medidor/app/views.py
@@ -14,19 +14,53 @@ from django.contrib import messages
 from smtplib import SMTPException
 
 # Create your views here.
+def clientes(request):
+    appr = Apr.objects.all()
+    data = {    
+        'apr': appr
+    }
+    return render(request, 'app/clientes.html',data)
 
-def apr(request):
+def apr(request, apr_id):
+    apppr = get_object_or_404(Apr, id=apr_id)
+    data = {    
+        'appr': apppr,
+        
+    }
 
 
-
-
-
-
-
-    return render(request, 'app/apr.html')
+    return render(request, 'app/apr.html',data)
 
 def home(request):
-    
+    try:
+        if request.method == 'POST':
+            name = request.POST['name']
+            email = request.POST['email']
+            phone = request.POST['phone']
+            ssr_apr = request.POST['ssr_apr']
+            cargo_apr = request.POST['cargo_apr']
+            comuna = request.POST['comuna']
+            cantidad = request.POST['cantidad']
+            fecha = datetime.datetime.now()
+            radio = request.POST['financiamiento']
+            message = request.POST['message']   
+
+            contacto = Contacto(name=name, email=email, phone=phone, ssr_apr=ssr_apr, cargo_apr=cargo_apr, comuna=comuna, cantidad=cantidad, fecha=fecha, radio=radio, message=message)
+            contacto.save()
+            inicio = settings.EMAIL_HOST_USER
+            password = settings.EMAIL_HOST_PASSWORD
+            info = f"nombre: {name} \n phone: {phone} \n ssr_apr: {ssr_apr} \n cargo_apr: {cargo_apr} \n comuna: {comuna} \n cantidad: {cantidad} \n fecha: {fecha} \n radio: {radio} \n message: {message}"
+            cc_recipients =['','']
+            try:
+                send_email('APR/SSR contacto', info, inicio, password, email, cc_recipients, 'smtp.gmail.com', 587)
+                messages.success(request, 'Tu mensaje ha sido enviado correctamente')
+            except send_email.error as e:
+                print(f"Error: Email could not be sent. {e}")   
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        messages.error(request, 'Hubo un problema con tu solicitud.')
+        return redirect('app/e404.html')  # Change to an appropriate error page
+
     return render(request, 'app/home.html')
 
 
@@ -59,9 +93,11 @@ def contacto(request):
     except Exception as e:
             print(f"Unexpected error: {e}")
             messages.error(request, 'Hubo un problema con tu solicitud.')
-            return redirect('app/404.html')  # Change to an appropriate error page
+            return redirect('app/e404.html')  # Change to an appropriate error page
 
     return render(request, 'app/contacto.html')
+
+
 
 def send_email(subject, message, sender_email, sender_password, recipient_email,cc_emails, smtp_server, smtp_port):
     try:    
@@ -82,8 +118,6 @@ def send_email(subject, message, sender_email, sender_password, recipient_email,
         print(f"Error: Email could not be sent. {e}")
 
 
-def clientes(request):
-    return render(request, 'app/clientes.html')
 
 
 def nosotros(request):
@@ -97,4 +131,6 @@ def soluciones(request):
 
 
 
+def e404(request):
+    return render(request, 'app/e404.html')
 
